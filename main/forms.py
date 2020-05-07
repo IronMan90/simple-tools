3edea510d397d3052724c6bc807bc515308ccdab
Masakrator <artur.olejak@gmail.com>
2020-05-07 22:09:00 +0200

    Added vcf, improved show results

--- main/forms.py
+++ main/forms.py
@@ -2,18 +2,19 @@ from flask_wtf import FlaskForm
 from wtforms import FieldList, FormField, StringField, IntegerField, SubmitField
 from wtforms.validators import NumberRange, DataRequired
 
+max_value = 10 ** 4
 
 class UsersForm(FlaskForm):
-    email = StringField('Email')
-    count = IntegerField('Counter', validators=[DataRequired(), NumberRange(min=1, max=10 ** 4)])
+    email = StringField('Domain')
+    count = IntegerField('Counter', validators=[DataRequired(), NumberRange(min=1, max=max_value)])
     submit = SubmitField('Generate')
 
 
 class VcfForm(FlaskForm):
-    count = IntegerField('Counter', validators=[DataRequired(), NumberRange(min=1, max=10 ** 4)])
+    count = IntegerField('Counter', validators=[DataRequired(), NumberRange(min=1, max=max_value)])
     submit = SubmitField('Generate')
 
 
 class SerialsForm(FlaskForm):
-    count = IntegerField('Counter', validators=[DataRequired(), NumberRange(min=1, max=10 ** 4)])
+    count = IntegerField('Counter', validators=[DataRequired(), NumberRange(min=1, max=max_value)])
     submit = SubmitField('Generate')
--- main/routes.py
+++ main/routes.py
@@ -1,15 +1,23 @@
+import random
+import string
+import os
+import glob
+import datetime
+
 import faker
-from flask import render_template, url_for, redirect
+from flask import render_template, url_for, redirect, send_file
 from main import app
 from main.forms import UsersForm, VcfForm, SerialsForm
 
 fake = faker.Faker()
 local_user = ''
+date_time = datetime.datetime.today().strftime("%M-%S-%f")
 
 
 class Results:
-    def __init__(self, test):
+    def __init__(self, test, vcf_):
         self.test = test
+        self.vcf_ = vcf_
 
     def get_test(self):
         return self.test
@@ -17,25 +25,62 @@ class Results:
     def set_test(self, test):
         self.test = test
 
+    def get_vcf(self):
+        return self.vcf_
 
-p = Results('')
+    def set_vcf(self, vcf_):
+        self.vcf_ = vcf_
+
+
+p = Results('', '')
+faker = faker.Faker(['it_IT', 'en_US', 'ja_JP'])
+
+
+def save_to_file(function, filename, extension="csv"):
+    with open(os.getcwd() + '/' + os.path.join('main/static') + '/' + filename + '.' + extension, 'w',
+              encoding="utf-8") as file:
+        file.write(f'{function}')
+        file.close()
 
 
 def generate_full_name():
     return fake.name()
 
 
-def generate_local_users(t):
+def generate_local_users(number, domain='example.com'):
     local_users = ''
-    for i in range(0, t):
+    for i in range(0, number):
         fullname = generate_full_name()
         names = fullname.split(' ')
-        email = f'{names[0].lower()}.{names[1].lower()}@proget.pl'
+        email = f'{names[0].lower()}.{names[1].lower()}@{domain}'
         nickname = f'{names[0][0].lower()}{names[1].lower()}'
         local_users += email + ';' + names[0] + ';' + names[1] + ';' + nickname + '\n'
-    local_user = local_users
-    p.set_test(local_user)
+    p.set_test(local_users)
     return local_users[:-1]
+
+
+def randstr():
+    randomstr = ''
+    for i in range(0, 10):
+        randomstr += random.choice(string.ascii_uppercase)
+    return randomstr
+
+
+def generate_vcf(number):
+    contacts = ''
+    for i in range(0, number):
+        fullname = faker.name().split(' ')
+        address = faker.address().split(',')[0]
+        contacts += f'BEGIN:VCARD\n' \
+            f'N:{fullname[1]};{fullname[0]}\n' \
+            f'FN:{fullname[0]} {fullname[1]}\n' \
+            f'ORG:{randstr()}\n' \
+            f'ADR;TYPE=WORK:{address}\n' \
+            f'TEL;TYPE=WORK,MSG:{random.randint(10 ** 6, 10 ** 7)}\n' \
+            f'EMAIL;TYPE=INTERNET:{fullname[1].lower()}@example.com\n' \
+            f'END:VCARD\n'
+    p.set_vcf(contacts)
+    return contacts
 
 
 @app.route('/')
@@ -49,14 +94,16 @@ def home():
 @app.route('/results')
 def results():
     t = p.get_test()
-    return render_template('users.html', t=t)
+    result_vcf = p.get_vcf()
+    # save_to_file(result_vcf, date_time, 'vcf')
+    return render_template('users.html', t=t, result_vcf=result_vcf)
 
 
 @app.route('/generate_users', methods=['GET', 'POST'])
 def user():
     form = UsersForm()
     if form.validate_on_submit():
-        generate_local_users(form.count.data)
+        generate_local_users(form.count.data, form.email.data)
         return redirect(url_for('results'))
     return render_template('home.html', form=form)
 
@@ -65,8 +112,8 @@ def user():
 def vcf():
     vcf_form = VcfForm()
     if vcf_form.validate_on_submit():
-        print(vcf_form.count.data)
-        return redirect(url_for('home'))
+        generate_vcf(vcf_form.count.data)
+        return redirect(url_for('results'))
     return render_template('home.html', vcf_form=vcf_form)
 
 
@@ -74,6 +121,5 @@ def vcf():
 def serials():
     serials_form = SerialsForm()
     if serials_form.validate_on_submit():
-        print(serials_form.count.data)
         return redirect(url_for('home'))
     return render_template('home.html', serials_form=serials_form)
